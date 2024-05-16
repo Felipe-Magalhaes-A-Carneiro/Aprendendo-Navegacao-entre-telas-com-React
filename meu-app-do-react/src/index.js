f1b8ea6b4dd062cdce7f3928b7222c9972717065
Felipe-Magalhaes-A-Carneiro <felipe.araujo.arq@outlook.com>
2024-05-16 20:15:14 -0300

    fix(index.js): rotas entre paginas criadas

--- meu-app-do-react/src/index.js
+++ meu-app-do-react/src/index.js
@@ -1,24 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-
-const App = () => {
-    return (
-        // container principal
-        <div className="container border rounded mt-2">
-
-            {/* linha para o título */}
-            <div className="row border-bottom m-2">
-                <h1 className="display-5 text-center">Seus pedidos</h1>
-            </div>
-
-
-        </div>
-    )
-
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
 ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    document.getElementById('root')
+  );
+  
